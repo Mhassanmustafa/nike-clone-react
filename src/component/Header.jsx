5ba9d73122af53f6e2b37316d0ef50c25edf3c66
Muhammad Hassan Mustafa <hbutt4832@gmail.com>
2021-06-30 01:14:27 +0500

    complete the top header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import ConverseSvg from "./svgs/ConverseSvg";
 import JordanSvg from "./svgs/JordanSvg";
+import "./css/Header.css";
 
 class NikeHeader extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { dropDownOpen: false };
   }
+
   render() {
     return (
       <>
@@ -21,6 +23,7 @@ class NikeHeader extends Component {
                   className="pre-brand-item d-sm-ib prl3-sm"
                   href="https://www.nike.com/jordan"
                   data-pre="ILink"
+                  id="Jordan"
                 >
                   <JordanSvg />
                 </a>
@@ -38,12 +41,49 @@ class NikeHeader extends Component {
                 </a>
               </li>
             </ul>
-            <div id="list-right">
+            {/* <div id="list-right">
               <a>Help</a>
               <span className="ml1-sm mr1-sm body-3">|</span>
               <a>Join Us</a>
               <span className="ml1-sm mr1-sm body-3">|</span>
               <a>Sign In</a>
+            </div> */}
+            <div className="header__nav">
+              <span className="header__option" id="help">
+                <p
+                  className="header__List"
+                  onMouseEnter={() => this.setState({ dropDownOpen: true })}
+                  onMouseLeave={() => {
+                    this.setState({ dropDownOpen: false });
+                  }}
+                >
+                  Help
+                </p>
+                <ul
+                  className={
+                    this.state.dropDownOpen
+                      ? "header__help-nav header__help-nav--open"
+                      : "header__help-nav header__help-nav--close"
+                  }
+                >
+                  <li className="header__title">Help</li>
+                  <li className="header__text">Order Status</li>
+                  <li className="header__text">Shipping & Delivery</li>
+                  <li className="header__text">Returns</li>
+                  <li className="header__text">Contact Us</li>
+                  <li className="header__text">Privacy Policy</li>
+                  <li className="header__text">Terms of Use</li>
+                  <li className="header__text">Send Us Feedback</li>
+                </ul>
+              </span>
+              <span className="headerBorder">|</span>
+              <span className="header__option" id="joinUs">
+                <p className="header__List">Join Us</p>
+              </span>
+              <span className="headerBorder">|</span>
+              <span className="header__option ">
+                <p className="header__List">Sign In</p>
+              </span>
             </div>
           </div>
         </div>
